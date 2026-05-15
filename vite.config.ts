ca8ecf509f869d762533a33ee8510dabc0945a7b
JDM-Github <jdmaster888@gmail.com>
2026-05-16 03:08:12 +0800

    Changes to be committed: 	modified:   .env.example 	modified:   src/lib/utilities/request_handler.ts 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import tailwindcss from "@tailwindcss/vite";
 import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
-	build: {
-		outDir: path.resolve(__dirname, '../backend/static'),
-		emptyOutDir: true,
-	},
 	server: {
 		proxy: {
 			'/api': 'http://localhost:5000'
